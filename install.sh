0ea4da263af138d2033da46c66d98204c860c6bd
Lucas Ou-Yang <lucasyangpersonal@gmail.com>
2014-02-26 02:26:58 -0800

    made sure install script works even if user has no starting vim directory

--- install.sh
+++ install.sh
@@ -2,24 +2,29 @@
 
 cwd=$(pwd)
 
-# Paste in our supertab (autocomplete) plugin
-cp ./plugin/supertab.vmb ~/.vim/plugin/supertab.vmb 
-
-# Install pathogen (recomended way to setup solarized)
+# Set up directory structure and install pathogen 
+# (recomended way to setup solarized)
 mkdir -p ~/.vim/autoload ~/.vim/bundle; \
+mkdir -p ~/.vim/doc ~/.vim/plugin; \
 curl -Sso ~/.vim/autoload/pathogen.vim \
     https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
 
-# Download vim-solarized
+# Paste in our supertab (autocomplete) plugin
+cp ./plugin/supertab.vim ~/.vim/plugin/.
+cp ./doc/supertab.txt ~/.vim/doc/.
+
 cd ~/.vim/bundle
+
+# Download vim-solarized
 git clone git://github.com/altercation/vim-colors-solarized.git
 
 # While we are in the bundle file, download syntastic
 git clone https://github.com/scrooloose/syntastic.git
 
-mkdir ~/.vim/colors
+mkdir -p ~/.vim/colors
 cp ~/.vim/bundle/vim-colors-solarized/colors/solarized.vim ~/.vim/colors/.
 
+# Move back to our project directory
 cd $cwd
 cat ./basic.vim > ~/.vimrc
 echo "Installed the Vim configuration successfully! Enjoy :-)"
